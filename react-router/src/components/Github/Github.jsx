bae7f757c5658fe26f062f6efe85ad148785a094
rahulstox <144617974+rahulstox@users.noreply.github.com>
2024-08-18 22:27:36 +0530

    first commit

---
+++ react-router/src/components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+  const [data, setData] = useState([])
+    useEffect(() => {
+        fetch('https://api.github.com/users/rahulstox')
+        .then(response => response.json)
+        .then(data => {
+          console.log(data);
+          setData(data)
+        })
+    }, [])
+  return (
+    <div className='text-3xl bg-gray-600 m-4 p-2 text-center text-white'> Github followers: {data.followers} 
+    <img className= 'text-center' src={data.avatar_url} alt="Git picture" width={200} />
+    </div>
+  )
+}
+
+export default Github
---
+++ react-router/src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider ,createBrowserRouter} from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:  <Layout/>,
+
+    children:[{
+      path:'',
+      element:<Home/>
+    },
+    {
+      path:'about',
+      element:<About/>
+
+    },
+    {
+      path:'contact',
+      element:<Contact/>
+
+    },
+    {
+      path:'User/:userid',
+      element:<User />
+
+    },
+    {
+      path:'Github',
+      element:<Github />
+    }
+  ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router = {router}/>
+  </StrictMode>,
+)
